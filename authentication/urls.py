a9e4665e1d5ce923379eef5338a89e6df18863d9
BelalMoawad <belalmoawad1999@gmail.com>
2022-11-06 18:22:17 +0200

    -django-extensions-knox-token-authentication

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
+    path('logoutall/', knox_views.LogoutAllView.as_view(), name='knox_logoutall'),
 ]
